621722418da5bd71f973d210059ac41d9a71b74b
jiao.shen <jiao.shen@qunar.com>
2016-08-07 22:06:04 +0800

    比较排序

--- sort.js
+++ sort.js
@@ -53,7 +53,7 @@ function insertionsort(arr) {
         var j = i,
             tmp = arr[i];
 
-        while (tmp < arr[j - 1]) {
+        while (j > 0 && tmp < arr[j - 1]) {
 
             arr[j] = arr[j - 1];
             j--;
